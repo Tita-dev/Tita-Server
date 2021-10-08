8d969aa5eefd39e91b2298b9ca30cb94447ce6e3
YongWoo <aba1430@naver.com>
2021-10-08 12:01:02 +0900

    [ADD] ForumDto

---
+++ src/main/java/com/example/jwt/dto/ForumDto.java
@@ -0,0 +1,12 @@
+package com.example.jwt.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class ForumDto {
+    private String forumName;
+    private String explanation;
+}
