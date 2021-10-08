f7cb1de9d8a6814da6ec8808e278196ac0a8ed3f
YongWoo <aba1430@naver.com>
2021-10-08 11:50:44 +0900

    [FIX] ForumController의 추가로 인한 EndPoint 변경

--- src/main/java/com/example/jwt/controller/MemberController.java
+++ src/main/java/com/example/jwt/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/tita")
+@RequestMapping("/tita/user")
 @RequiredArgsConstructor
 public class MemberController {
 
