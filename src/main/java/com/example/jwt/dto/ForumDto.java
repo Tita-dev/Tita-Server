3baaf0ffad4e0049ff7edf83bfe298bf3458ed0c
YongWoo <aba1430@naver.com>
2021-10-20 23:05:03 +0900

    [ADD] ForumDto에 빌더 추가

--- src/main/java/com/example/jwt/dto/ForumDto.java
+++ src/main/java/com/example/jwt/dto/ForumDto.java
@@ -1,12 +1,31 @@
 package com.example.jwt.dto;
 
+import com.example.jwt.domain.Forum;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 
 @Getter
 @NoArgsConstructor
+@Builder
+@Setter
 public class ForumDto {
     private String forumName;
     private String explanation;
+
+
+    public ForumDto (String forumName, String explanation){
+        this.forumName = forumName;
+        this.explanation = explanation;
+    }
+
+    public Forum toEntity(){
+        return Forum.builder()
+                .forumName(forumName)
+                .explanation(explanation)
+                .build();
+    }
+
 }
