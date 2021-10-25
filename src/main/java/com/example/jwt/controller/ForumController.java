8da1813f1055df61323a8dbbbe549a46f21d461d
YongWoo <aba1430@naver.com>
2021-10-25 20:20:15 +0900

    [ADD] View ForumList

--- src/main/java/com/example/jwt/controller/ForumController.java
+++ src/main/java/com/example/jwt/controller/ForumController.java
@@ -5,8 +5,11 @@ import com.example.jwt.domain.response.ListResult;
 import com.example.jwt.domain.response.ResponseService;
 import com.example.jwt.dto.ForumChangeDto;
 import com.example.jwt.dto.ForumDto;
+import com.example.jwt.service.ForumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tita/forum")
@@ -14,14 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class ForumController {
 
     private final ResponseService responseService;
+    private final ForumService forumService;
 
     @GetMapping("/list")
-    public ListResult<String> forumList()throws Exception {
-        /*
-            Forum Service 제작 후 forumList 불러와 전달
-                + 예외처리
-         */
-        return responseService.getListResult(null);
+    public ListResult<ForumDto> forumList()throws Exception {
+        List<ForumDto> forumDtoList = forumService.getForumList();
+        return responseService.getListResult(forumDtoList);
     }
 
     @PostMapping("/create")
