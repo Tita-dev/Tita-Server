d5f120ddac3677f5f1f83d722fcf967256f8a259
YongWoo <aba1430@naver.com>
2021-10-25 20:24:57 +0900

    [ADD] Forum Repo

--- src/main/java/com/example/jwt/repository/ForumRepository.java
+++ src/main/java/com/example/jwt/repository/ForumRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface ForumRepository extends JpaRepository<Forum, Long> {
+    Forum findByForumName(String forumName);
+    Forum deleteByForumName(String forumName);
 }
