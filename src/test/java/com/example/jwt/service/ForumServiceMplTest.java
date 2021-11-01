14f8aa1385fe1f891b9b40f2318d931734480247
YongWoo <aba1430@naver.com>
2021-11-01 20:16:26 +0900

    [ADD] CreateTest

---
+++ src/test/java/com/example/jwt/service/ForumServiceMplTest.java
@@ -0,0 +1,29 @@
+package com.example.jwt.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForumServiceMplTest {
+
+    @DisplayName("게시판 목록 구현")
+    @Test
+    void getForumList() {
+    }
+
+    @DisplayName("게시판 생성")
+    @Test
+    void forumCreate() {
+    }
+
+    @DisplayName("게시판 삭제")
+    @Test
+    void forumDelete() {
+    }
+
+    @DisplayName("게시판 수정")
+    @Test
+    void forumPut() {
+    }
+}
