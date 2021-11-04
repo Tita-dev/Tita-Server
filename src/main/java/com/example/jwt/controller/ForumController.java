da89c00a92255e32566f930c55fadfd0ed033b49
YongWoo <aba1430@naver.com>
2021-11-04 21:08:03 +0900

    [ADD] ForumController Template

--- src/main/java/com/example/jwt/controller/ForumController.java
+++ src/main/java/com/example/jwt/controller/ForumController.java
@@ -5,6 +5,7 @@ import com.example.jwt.domain.response.ListResult;
 import com.example.jwt.domain.response.ResponseService;
 import com.example.jwt.dto.ForumChangeDto;
 import com.example.jwt.dto.ForumDto;
+import com.example.jwt.dto.PostDto;
 import com.example.jwt.service.ForumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -40,6 +41,28 @@ public class ForumController {
     @PutMapping("/put")
     public CommonResult forumPut(@RequestBody ForumChangeDto forumChangeDto) throws Exception{
         forumService.forumPut(forumChangeDto);
+        return responseService.getSuccessResult();
+    }
+
+    @GetMapping("/{forumName}/list")
+    public List<String> postList()throws Exception{
+
+        return null;
+    }
+
+    @PostMapping("/{forumName}/create")
+    public CommonResult postCreate(@PathVariable String forumName, @RequestBody PostDto postDto) throws Exception{
+
+        return responseService.getSuccessResult();
+    }
+
+    @DeleteMapping("/{forumName}/delete")
+    public CommonResult postDelete()throws Exception{
+        return responseService.getSuccessResult();
+    }
+
+    @PutMapping("/{forumName}/put")
+    public CommonResult postPut()throws Exception{
         return responseService.getSuccessResult();
     }
 }
