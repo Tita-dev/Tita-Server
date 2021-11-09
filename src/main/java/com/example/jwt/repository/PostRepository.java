36ff01edc87c252e17b0dac5719e7540d5debbaf
YongWoo <aba1430@naver.com>
2021-11-09 17:56:40 +0900

    [ADD] PostRepo

--- src/main/java/com/example/jwt/repository/PostRepository.java
+++ src/main/java/com/example/jwt/repository/PostRepository.java
@@ -3,9 +3,15 @@ package com.example.jwt.repository;
 import com.example.jwt.domain.Forum;
 import com.example.jwt.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
     List<Post> findAllByForum (Forum forum);
+    @Transactional
+    void deletePostByPostNameAndForum(String postName,Forum forum);
+
+    Post findByPostNameAndForum(String postName,Forum forum);
+
 }
