15c1c96f32ca76f1e78cf1b15740ebc456428ba6
YongWoo <aba1430@naver.com>
2021-11-13 08:16:01 +0900

    [ADD] Comments Controller

--- src/main/java/com/example/jwt/controller/CommentsController.java
+++ src/main/java/com/example/jwt/controller/CommentsController.java
@@ -4,6 +4,7 @@ import com.example.jwt.domain.response.CommonResult;
 import com.example.jwt.domain.response.ListResult;
 import com.example.jwt.domain.response.ResponseService;
 import com.example.jwt.dto.CommentsDto;
+import com.example.jwt.service.CommentsService;
 import com.example.jwt.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,28 +18,23 @@ public class CommentsController {
 
     private final ResponseService responseService;
     private final PostService postService;
+    private final CommentsService commentsService;
 
     @GetMapping("/{forumName}/{postIdx}")
-    public ListResult<String> getPostAndComments (@PathVariable String forumName, @PathVariable Long postIdx) throws Exception{
-        /*
-            게시글의 내용과 댓글의 내용을 같이 담는 DTO를 만들어 List형태로 return
-         */
-        return responseService.getListResult(null);
+    public ListResult<String> getPostAndComments (@PathVariable Long postIdx) throws Exception{
+        List<String> postAndCommentsDto = commentsService.getPostAndComments(postIdx);
+        return responseService.getListResult(postAndCommentsDto);
     }
 
     @PostMapping("/{forumName}/{postIdx}/create")
-    public CommonResult commentsCreate (@PathVariable String forumName, @PathVariable Long postIdx ,@RequestBody CommentsDto commentsDto) throws Exception{
-        /*
-            forumName, postIdx에 해당하는 정보를 매핑하여 Dto에 담아 만든다.
-         */
+    public CommonResult commentsCreate (@PathVariable Long postIdx ,@RequestBody CommentsDto commentsDto) throws Exception{
+        commentsService.commentsCreate(postIdx,commentsDto);
         return responseService.getSuccessResult();
     }
 
-    @DeleteMapping("/{forumName}/{postIdx}/create")
-    public CommonResult postDelete(@PathVariable String forumName,@PathVariable Long postIdx ,@RequestBody CommentsDto commentsDto)throws Exception{
-        /*
-
-         */
+    @DeleteMapping("/{forumName}/{postIdx}/delete")
+    public CommonResult postDelete(@PathVariable Long postIdx ,@RequestBody CommentsDto commentsDto)throws Exception{
+        commentsService.commentsDelete(postIdx,commentsDto);
         return responseService.getSuccessResult();
     }
 }
