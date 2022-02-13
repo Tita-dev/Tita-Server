be43d920e1e31f2c13a0d58eff7f421d99679832
YongWoo <aba1430@naver.com>
2022-02-13 10:00:11 +0900

    [ADD] PostCustomRepo faild

--- src/main/java/com/example/jwt/model/post/repository/PostRepository.java
+++ src/main/java/com/example/jwt/model/post/repository/PostRepository.java
@@ -2,6 +2,8 @@ package com.example.jwt.model.post.repository;
 
 import com.example.jwt.model.forum.Forum;
 import com.example.jwt.model.post.Post;
+import com.example.jwt.model.post.dto.PostDto;
+import com.example.jwt.model.post.dto.PostResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,4 +24,5 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRep
 
     List<Post> findTop5ByOrderByPostLikeList();
 
+    //List<PostResponseDto> findPostPostLikeOrder();
 }
