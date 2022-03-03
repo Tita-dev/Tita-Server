010149f8383c6ddbf37d1f26543b5b703a9fb177
YongWoo <aba1430@naver.com>
2022-03-03 17:29:39 +0900

    [ADD] 토큰의 정보를 못가져오는 오류 해결중

--- src/main/java/com/example/jwt/config/security/SecurityConfiguration.java
+++ src/main/java/com/example/jwt/config/security/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.example.jwt.config.security;
 
+import com.example.jwt.config.security.auth.MyUserDetails;
+import com.example.jwt.config.security.jwt.JwtRequestFilter;
+import com.example.jwt.config.security.jwt.JwtTokenFilterConfigurer;
+import com.example.jwt.config.security.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +12,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -15,6 +21,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity //(debug = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final MyUserDetails myUserDetails;
+
+    private final JwtUtil jwtUtil;
 
     @Override
     protected void configure(HttpSecurity  http) throws Exception{
@@ -27,16 +37,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .antMatchers("/tita/forum/**").hasAuthority("ROLE_STUDENT")
+                //.hasAnyRole("ROLE_STUDENT","ROLE_DORMITORY_MANAGER","ROLE_SCHOOL_MANAGER","ROLE_SCHOOL_ADMIN")
                 .antMatchers("/tita/admin/authorization/**").hasAuthority("ROLE_SCHOOL_ADMIN")
                 .antMatchers("/tita/**").permitAll()
-                .antMatchers("/swagger-resources/**").permitAll()
                 .anyRequest().authenticated();
+        http.userDetailsService(myUserDetails);
+
+        http.apply(new JwtTokenFilterConfigurer(jwtUtil));
     }
 
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers("/h2-console/**", "/tita/**", "/exception/**")
+                .antMatchers("/h2-console/**", "/exception/**")
                 .antMatchers("/swagger-resources/**")
                 .antMatchers("/swagger-ui.html")
                 .antMatchers("/swagger/**")
