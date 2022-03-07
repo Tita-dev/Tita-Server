1881be2be41b4b8395574bad49127350ade63144
YongWoo <aba1430@naver.com>
2022-03-07 12:22:33 +0900

    [ADD] 변경과정중 Dto의 추가가 필요하여 생성함

---
+++ src/main/java/com/example/jwt/model/post/dto/PostDeleteDto.java
@@ -0,0 +1,29 @@
+package com.example.jwt.model.post.dto;
+
+import com.example.jwt.model.post.Post;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PostDeleteDto {
+    @NotBlank
+    @Size(max = 100)
+    private String postName;
+
+    @Size(max = 500, min = 0)
+    private String content;
+
+    public Post toEntity() {
+        return Post.builder()
+                .postName(this.getPostName())
+                .content(this.getContent())
+                .build();
+    }
+}
